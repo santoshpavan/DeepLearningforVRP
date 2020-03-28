40b56a2c49306232db4740f39b76c2b68c2003f8
santoshpavan <santoshpavan.psp@gmail.com>
2020-03-28 16:34:04 -0400

    wokring animation of edges

--- visualize_data.py
+++ visualize_data.py
@@ -12,21 +12,17 @@ import time
 
 # getting data from tsplib95 data
 problem = tsplib95.load_problem('Vrp-All/A/A-n32-k5.vrp')
-# is a networkx graph
 fig, ax = plt.subplots(figsize=(10,7))
 G = problem.get_graph()
 edge_list = list(problem.get_edges())
 e = []
-i = 0
+# to keep the position of the nodes in the plot fixed
 pos = nx.spring_layout(G)
 def update(num):
-    # for edge in edge_list:
     ax.clear()
-    #global i
     e.append(edge_list[num+1])
     nx.draw(G, pos=pos, with_labels = True, edgelist=e, edge_color='red')
     ax.set_title("Frame %d:    "%(num+1), fontweight="bold")
-    #i = i+1
-    #print(num)
+
 ani = FuncAnimation(fig, update, frames=len(edge_list))
 plt.show()
