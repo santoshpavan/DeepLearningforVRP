cac279488628a81c31d066feeda3882f1cec4be4
santoshpavan <santoshpavan.psp@gmail.com>
2020-04-15 21:14:17 -0400

    realizing the graph from nx graph

--- visualize_data_nx_draw.py
+++ visualize_data_nx_draw.py
@@ -20,7 +20,7 @@ node_list = list(problem.get_nodes())
 node_color_values = [(0,0,1) for i in range(len(node_list))]
 # to keep the position of the nodes in the plot fixed
 pos = problem.node_coords
-
+"""
 def update(num):
     ax.clear()
     if num<=len(node_list):
@@ -30,3 +30,13 @@ def update(num):
 
 ani = FuncAnimation(fig, update, frames=len(edge_list))
 plt.show()
+"""
+import io
+im_io = io.BytesIO()
+#nx.draw(G)
+nx.draw(G, pos=pos, with_labels = False, node_color=(1,0,0), node_size=10, edge_color=(1,1,1))
+fig.savefig(im_io, format='png',dpi=10)
+im = Image.open(im_io)
+#im = im.resize((10,10))
+im.show()
+print(im.size)
